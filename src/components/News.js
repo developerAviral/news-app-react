7f2f8c8fe43072bab7236dcd8c2789ba0ddbb676
Developer Avi <developer.aviral@gmail.com>
2023-08-26 20:56:49 +0530

    Adding PropTypes and Default Props

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  static defaultProps ={
+    country: 'in',
+    pageSize: 6,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
   constructor() {
     super();
     this.state = {
@@ -12,10 +26,11 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f960c2cb21be445dbd8241a124fe7aa0&page=1&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f960c2cb21be445dbd8241a124fe7aa0&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false});
   }
 
   handleNextClick = async () => {
@@ -24,19 +39,21 @@ export class News extends Component {
 
     }
     else {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f960c2cb21be445dbd8241a124fe7aa0&page=${this.state.page + 1}&pageSize=20`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f960c2cb21be445dbd8241a124fe7aa0&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json();
-      this.setState({ page: this.state.page + 1, articles: parsedData.articles });
+      this.setState({ page: this.state.page + 1, articles: parsedData.articles, loading: false });
     }
     console.log(this.state.page);
   }
 
   handlePreviousClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f960c2cb21be445dbd8241a124fe7aa0&page=${this.state.page - 1}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f960c2cb21be445dbd8241a124fe7aa0&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, page: this.state.page - 1 });
+    this.setState({ articles: parsedData.articles, page: this.state.page - 1, loading: false });
 
   }
 
@@ -44,10 +61,10 @@ export class News extends Component {
     return (
 
       <div className='container my-3'>
-        <h2>Aviral's News App - Top Headlines</h2>
-
-        <div className="row">
-          {this.state.articles.map((element) => {
+        <h2 className="text-center">Aviral's News App - Top Headlines</h2>
+        {this.state.loading && <Spinner />}
+        <div className="row my-2">
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
 
               <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage}
@@ -59,7 +76,7 @@ export class News extends Component {
 
         <div className='container d-flex justify-content-between'>
           <button disabled={this.state.page <= 1} className="btn btn-primary mx-2" onClick={this.handlePreviousClick}>&larr; Previous</button>
-          <button className="btn btn-primary mx-2" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} className="btn btn-primary mx-2" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
 
       </div>
